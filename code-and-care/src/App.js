c6030233a3a376b9afabe3da7ff383e0b96b7e82
Allison Faustino <allisondark01@gmail.com>
2025-03-17 21:59:05 -0300

    Puxei da main

--- code-and-care/src/App.js
+++ code-and-care/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
-}
+
+    <Topbar></Topbar>
+    
+};
 
 export default App;
---
+++ code-and-care/src/components/Topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Topbar(){
+
+    return(
+        <h1>Ola mundo</h1>
+    );
+   
+};
+
+export default Topbar;
