cbb2d9927197ac4c23c7c6186ceca8c64f2940f9
Walter Fonseca <walterfonseca1606@gmail.com>
2025-04-10 13:46:58 -0300

    Arrumando MapaRotas

--- code-and-care/src/routes/Rotas.js
+++ code-and-care/src/routes/Rotas.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound404 from '../pages/NotFound404/NotFound404';
 import Home from '../pages/Home/Home';
+import MapaRotas from '../pages/MapaRotas/MapaRotas';
 function Rotas(){
     return (
         <BrowserRouter>
             <Routes>
 
                 <Route path="/" element={<Home/>} />
-
+                <Route path="/mapa-rotas" element={<MapaRotas/>} />
                 <Route path="*" element={<NotFound404/>}/>
+                
             </Routes>
         </BrowserRouter>
     )
