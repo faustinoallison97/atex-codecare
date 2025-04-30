b545d1ae80cd92c533d3166f94a487b79de7a3b7
Walter Fonseca <walterfonseca1606.dev@gmail.com>
2025-04-29 23:49:50 -0300

    Criando e ajustando menu hamburguer

--- code-and-care/src/components/Topbar/Topbar.jsx
+++ code-and-care/src/components/Topbar/Topbar.jsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Topbar.module.css";
 import logo from "../../assets/LogoCode&CareBranco.png";
 import { useNavigate } from "react-router";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 function Topbar() {
   const navigate = useNavigate();
+  const [menuAberto, setMenuAberto] = useState(false);
 
   function navegarHome() {
     navigate("/");
+    setMenuAberto(false);
   }
   function navegarMapaRotas() {
     navigate("/mapa-rotas");
+    setMenuAberto(false);
   }
   function navegarSessao2() {
     window.location.href = "/#sessao2";
+    setMenuAberto(false);
   }
   function navegarSessao3() {
     window.location.href = "/#sessao3";
+    setMenuAberto(false);
   }
   function navegarSessao4() {
     window.location.href = "/#sessao4";
+    setMenuAberto(false);
   }
-  
 
   return (
-    <nav className={style.containernav}>
-      <img onClick={navegarHome} src={logo} alt="" />
-      <ul>
-        <li onClick={navegarMapaRotas}>Rotas</li>
-        <li onClick={navegarSessao2}>Sobre o Projeto</li>
-        <li onClick={navegarSessao3}>Como Funciona</li>
-        <li onClick={navegarSessao4}>Contato</li>
-      </ul>
-    </nav>
+    <>
+      <nav className={style.containernav}>
+        <img onClick={navegarHome} src={logo} alt="Logo" />
+        <ul className={style.menuDesktop}>
+          <li onClick={navegarMapaRotas}>Rotas</li>
+          <li onClick={navegarSessao2}>Sobre o Projeto</li>
+          <li onClick={navegarSessao3}>Como Funciona</li>
+          <li onClick={navegarSessao4}>Contato</li>
+        </ul>
+        <button
+          className={style.menuToggle}
+          onClick={() => setMenuAberto(!menuAberto)}
+        >
+          {menuAberto ? <FaTimes /> : <FaBars />}
+        </button>
+      </nav>
+
+      {menuAberto && (
+        <div className={style.menuMobile}>
+          <ul>
+            <li onClick={navegarMapaRotas}>Rotas</li>
+            <li onClick={navegarSessao2}>Sobre o Projeto</li>
+            <li onClick={navegarSessao3}>Como Funciona</li>
+            <li onClick={navegarSessao4}>Contato</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
